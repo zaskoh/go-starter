bd1ba4fc4442cf89ef7ae9b43c6baa9472b27ba9
zaskoh <117531688+zaskoh@users.noreply.github.com>
2022-11-29 17:56:40 +0100

    added check if config exists and fallback to only envs plus optional flag passed filter possibility

--- config/config.go
+++ config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"context"
+	"flag"
 	"os"
 	"os/signal"
 	"syscall"
@@ -9,7 +10,13 @@ import (
 	"github.com/ilyakaznacheev/cleanenv"
 )
 
+var (
+	configPath = flag.String("config", "config.yml", "path to config file")
+	_          = flag.Bool("flag-to-deactivate", false, "additional flags that can be handled via isFlagPassed")
+)
+
 func init() {
+	flag.Parse()
 	err := loadConfiguration()
 	if err != nil {
 		os.Exit(1)
@@ -22,7 +29,8 @@ type loadConfig struct {
 }
 
 type baseConfig struct {
-	LogLevel string `yaml:"log_level" env:"LOG_LEVEL" env-default:"debug"`
+	LogLevel         string `yaml:"log_level" env:"LOG_LEVEL" env-default:"debug"`
+	FlagToDeactivate bool
 }
 
 type anotherConfig struct {
@@ -39,11 +47,22 @@ var AnotherExample anotherConfig
 
 func loadConfiguration() error {
 	var confLoad loadConfig
-	if err := cleanenv.ReadConfig("./config.yml", &confLoad); err != nil {
-		return err
+
+	if _, err := os.Stat(*configPath); err == nil {
+		// if we have a config, load
+		if err := cleanenv.ReadConfig(*configPath, &confLoad); err != nil {
+			return err
+		}
+	} else {
+		// if config.yml doesnt exist, we just load the env vars
+		if err := cleanenv.ReadEnv(&confLoad); err != nil {
+			return err
+		}
 	}
 
 	Base = confLoad.BaseConfig
+	Base.FlagToDeactivate = !isFlagPassed("flag-to-deactivate")
+
 	AnotherExample = confLoad.AnotherConfig
 	return nil
 }
@@ -65,4 +84,14 @@ func CreateLaunchContext() (context.Context, func(), chan bool) {
 		close(canceledChanChan)
 	}
 	return ctx, cancel, canceledChanChan
+}
+
+func isFlagPassed(name string) bool {
+	found := false
+	flag.Visit(func(f *flag.Flag) {
+		if f.Name == name {
+			found = true
+		}
+	})
+	return found
 }
